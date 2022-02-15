49689aa919cc6eecf95be32f790978926e51b816
Eric Stautmeister <71490551+Extensus@users.noreply.github.com>
2022-02-15 11:11:54 +0100

    update

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,5 @@
+// @flow
+
 import React, { Component } from 'react'; //useEffect, useState
 import ReactDOM from 'react-dom';
 import {
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// @flow
+
 const express = require('express');
 require('dotenv').config();
 
