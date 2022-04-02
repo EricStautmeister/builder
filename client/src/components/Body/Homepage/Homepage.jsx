e37f493bc980af8a60ad5475c8073a6ea488acb8
Eric Stautmeister <71490551+Extensus@users.noreply.github.com>
2022-04-02 22:18:27 +0200

    remove modal click bug from core page

--- client/src/components/Body/Homepage/Homepage.jsx
+++ client/src/components/Body/Homepage/Homepage.jsx
@@ -6,15 +6,27 @@ import { Core, Edit } from './modes';
 import '../../styling/css/Homepage.css';
 import { Header } from '../../Header';
 import { Footer } from '../../Footer';
+import { WINDOWS } from './constants';
 
 export default function Homepage() {
     const [Items, setItems] = useState(buildComponents);
     const [mode, setMode] = useState(MODES.CORE);
     const [modal, setModal] = useState(false);
     const [modalData, setModalData] = useState();
+    
+    /**
+     * * If the display context is the main window and the mode is edit, then if the modal is false,
+     * set the modal data to the data passed in. Then set the modal to true
+     * @param toggleData - The data that is passed to the toggle function.
+     * @returns The `toggle` function returns a `boolean` value. This is used to determine whether or
+     * not the modal should be displayed.
+     */
     const toggle = (toggleData) => {
-        if (modal === false) setModalData(toggleData.data);
-        setModal(!modal);
+        if (toggleData.displayContext === WINDOWS.MAINWINDOW && mode === MODES.EDIT) {
+            if (modal === false) setModalData(toggleData.data);
+            setModal(!modal);
+            return;
+        }
     };
 
     /**
--- client/src/components/Body/Homepage/MovableItem.jsx
+++ client/src/components/Body/Homepage/MovableItem.jsx
@@ -7,6 +7,12 @@ export default function MovableItem({ Item, setItems, onClick }) {
     const data = Item.data;
     const position = Item.position;
 
+    /**
+     * Given a current item and a display context, change the display context of the current item to
+     * the display context
+     * @param currentItem - The item that is currently being dragged.
+     * @param displayContext - The display context of the item.
+     */
     const changePosition = (currentItem, displayContext) => {
         setItems((previousState) => {
             return previousState.map((previousItem) => {
@@ -21,6 +27,11 @@ export default function MovableItem({ Item, setItems, onClick }) {
         });
     };
 
+    /* This is the code that allows the item to be dragged. The `useDrag` hook is used to create a
+    draggable component. The `item` property is the item that is being dragged. The `type` property
+    is the type of item that is being dragged. The `end` property is the function that is called
+    when the item is dropped. The `collect` property is the function that is called when the item is
+    being dragged. */
     const [{ isDragging }, drag] = useDrag({
         item: { name: data.name, left: position.left, top: position.top },
         type: ItemTypes.CORE,
@@ -40,10 +51,11 @@ export default function MovableItem({ Item, setItems, onClick }) {
 
     const opacity = isDragging ? 0.6 : 1;
 
+    
     const options = {
         ref: drag,
         style: { opacity },
-        onClick: () => onClick({ data })
+        onClick: () => onClick({ data, displayContext: Item.displayContext }),
     };
 
     return <>{Item.jsx(options.ref, options.style, options.onClick)}</>;
